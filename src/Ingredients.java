502acb5cee69f02b51b81f5718888a920e15c5d5
Nils Schumacher <nis@click-solutions.de>
2023-10-25 10:28:38 +0200

    feat: add ingredients and functions for get and create a ingredient

---
+++ src/Ingredients.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class Ingredients {
+    private int ingredientId;
+    private String name;
+    private DatabaseConnection db = DatabaseConnection.getInstance();
+    private Connection conn = db.getConnection();
+
+    public Ingredients(int ingredientId) {
+        this.ingredientId = ingredientId;
+        ResultSet resultSet = db.executeQuery("SELECT * FROM ingredients WHERE ingredientId = " + ingredientId);
+        try {
+            while (resultSet.next()) {
+                this.name = resultSet.getString("name");
+            }
+        } catch (SQLException e) {
+            db.printSQLException(e);
+        }
+    }
+
+    static boolean createIngredients(String name) {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        Connection conn = db.getConnection();
+
+        // Create a new ingredients in the database
+        try {
+            conn.createStatement().executeUpdate("INSERT INTO ingredients (name) " +
+                    "VALUES ('" + name + "')");
+            return true;
+        } catch (SQLException e) {
+            db.printSQLException(e);
+            return false;
+        }
+    }
+
+    public void print() {
+        System.out.println("Ingredient ID: " + this.ingredientId);
+        System.out.println("Name: " + this.name);
+    }
+}
