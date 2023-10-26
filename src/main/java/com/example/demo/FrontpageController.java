e3dd3717e0ea3a9a61dfd54a9a728afd8038a0d5
RBB-TECHNIK-PC-\Schueler-ADM <philipp.roettgers@gmail.com>
2023-10-26 12:53:24 +0200

    add images to frontpage items

--- src/main/java/com/example/demo/FrontpageController.java
+++ src/main/java/com/example/demo/FrontpageController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
@@ -7,6 +9,8 @@ import com.example.demo.model.Recipe;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -31,11 +35,20 @@ public class FrontpageController {
                 Recipe recipe = new Recipe(resultSet.getInt("recipeId"));
                 VBox itemWrapper = new VBox();
                 itemWrapper.getStyleClass().add("item-wrapper");
+
+                ImageView imageView = new ImageView();
+                Image image = new Image(MainApplication.class.getResource("/assets/" + recipe.getPicture()).toString());
+                imageView.setImage(image);
+                imageView.fitWidthProperty().bind(itemWrapper.widthProperty());
+                imageView.setPreserveRatio(true);
+
                 Label name = new Label();
                 name.setText(recipe.getName());
                 name.setMaxWidth(Double.MAX_VALUE);
                 name.setAlignment(Pos.CENTER);
                 name.getStyleClass().add("item-name");
+
+                itemWrapper.getChildren().add(imageView);
                 itemWrapper.getChildren().add(name);
                 items.add(itemWrapper, count, rowCount);
 
