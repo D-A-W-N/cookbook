76c15e813853e82a8b9605cabb7ca10d7d1518c6
DavidL01 <lamdav042@gmail.com>
2023-11-09 10:46:38 +0100

    Detailpage erweitert

--- src/main/java/com/example/demo/DetailPageController.java
+++ src/main/java/com/example/demo/DetailPageController.java
@@ -4,35 +4,76 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+
 import com.example.demo.model.Recipe;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 
-public class DetailPageController {
+public class DetailPageController{
+    @FXML
+    public Label mealDescription;
+    @FXML
+    private Label instructionsLabel;
+    @FXML
+    private Label recipeName;
 
-    public String getInstructionForRecipe(int recipeId) throws SQLException {
+    public DetailPageController(){
+    }
+
+    public String[] getInstructionForRecipe(int recipeId) throws SQLException {
         DatabaseConnection db = DatabaseConnection.getInstance();
         Connection conn = db.getConnection();
 
-        // Ersetzen Sie "recipeId" durch den Wert der Rezept-ID, die Sie abrufen möchten.
-        String query = "SELECT instructions FROM recipes WHERE recipe_id = 6";
-        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        String queryInstructions = "SELECT instructions FROM recipes WHERE recipeId = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(queryInstructions);
+
+        String queryRecipeName ="SELECT name FROM recipes WHERE recipeId = ?";
+        PreparedStatement preparedStatement1 = conn.prepareStatement(queryRecipeName);
+
+        String queryMealDescription ="SELECT description FROM recipes WHERE recipeId = ?";
+        PreparedStatement preparedStatement2 = conn.prepareStatement(queryMealDescription);
+
+        //String queryIngredients ="SELECT "
         preparedStatement.setInt(1, recipeId);
+        preparedStatement1.setInt(1, recipeId);
+        preparedStatement2.setInt(1, recipeId);
 
         ResultSet resultSet = preparedStatement.executeQuery();
+        ResultSet resultSet1 = preparedStatement1.executeQuery();
+        ResultSet resultSet2 = preparedStatement2.executeQuery();
 
-        if (resultSet.next()) {
-            // Wenn ein Ergebnis gefunden wurde, holen Sie die Zubereitungsanweisung.
-            String instruction = resultSet.getString("instructions");
-            System.out.println(instruction);
-            return instruction;
+        if (resultSet.next() && resultSet1.next() && resultSet2.next()) {
+
+            String instructions = resultSet.getString("instructions");
+            String recipeName = resultSet1.getString("name");
+            String mealDescription = resultSet2.getString("description");
+
+            /*Label instructions = new Label();
+            instructions.setText(resultSet.getString("instructions"));*/
+            System.out.println(instructions);
+            return new String[]{instructions , recipeName, mealDescription};
         } else {
             // Keine Übereinstimmung gefunden.
-            return null;
+            return new String[]{"keine Anweisung gefunden"};
         }
     }
 
+    public void initialize() {
+        try {
+            String recipeInstructions = getInstructionForRecipe(4)[0];
+            String recipeName = getInstructionForRecipe(4)[1];
+            String mealDescription = getInstructionForRecipe(4)[2];
+
+
+            if (recipeInstructions != null && recipeName != null && mealDescription != null) {
+                this.instructionsLabel.setText(recipeInstructions);
+                this.recipeName.setText(recipeName);
+                this.mealDescription.setText(mealDescription);
+            } else {
+                instructionsLabel.setText("Das Rezept konnte aus unerklärlichen Gründen nicht geladen werden");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
