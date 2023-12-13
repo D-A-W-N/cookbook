0e5551b67bd46baee1a58673e7f6f0915273a5dd
DavidL01 <lamdav042@gmail.com>
2023-12-13 11:00:58 +0100

    Design + Animation

--- src/main/java/com/example/demo/FrontpageController.java
+++ src/main/java/com/example/demo/FrontpageController.java
@@ -8,14 +8,17 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import com.example.demo.model.Ingredient;
 import com.example.demo.model.Recipe;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
@@ -23,14 +26,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class FrontpageController {
     @FXML
-    private Label welcomeText;
+    private Label hoveredRecipeName;
 
     @FXML
     private GridPane items;
@@ -39,9 +42,69 @@ public class FrontpageController {
     public TextField textfield;
 
     @FXML
+    private ImageView randomPic;
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
     public void initialize() {
         DatabaseConnection db = DatabaseConnection.getInstance();
         Connection conn = db.getConnection();
+
+        int randomRecipeId = Recipe.getRandomRecipeId();
+        Recipe randomRecipe = new Recipe(randomRecipeId);
+
+        String recipeName = randomRecipe.getName();
+        String recipePicture = randomRecipe.getPicture();
+
+        Image recipeOfDayPic = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/assets/" + recipePicture)));
+        this.randomPic.setImage(recipeOfDayPic);
+
+        this.hoveredRecipeName.setText("Gericht des Tages!\n" + recipeName);
+
+        stackPane.setOnMouseEntered(event -> {
+            stackPane.setVisible(false);
+            hoveredRecipeName.setVisible(true);
+
+            slideUp(hoveredRecipeName);
+        });
+
+        hoveredRecipeName.setOnMouseClicked(event -> {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/recipe-view.fxml"));
+            Parent detailRoot = null;
+            try {
+                detailRoot = loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            DetailPageController detailPageController = loader.getController();
+
+            int selectedRecipeId = randomRecipe.getRecipeId();
+            detailPageController.initialize(selectedRecipeId);
+
+            try {
+                detailPageController.getInstructionForRecipe(selectedRecipeId);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            Scene detailScene = new Scene(detailRoot, 1280, 720);
+            Stage primaryStage = (Stage) hoveredRecipeName.getScene().getWindow();
+            primaryStage.setScene(detailScene);
+            primaryStage.show();
+        });
+
+
+        hoveredRecipeName.setOnMouseExited(event -> {
+            slideDown(hoveredRecipeName);
+            hoveredRecipeName.setVisible(false);
+            stackPane.setVisible(true);
+        });
+
+        System.out.println(recipeName);
+        System.out.println(recipePicture);
+
 
 
         try {
@@ -49,7 +112,8 @@ public class FrontpageController {
             int count = 0;
             int rowCount = 0;
 
-            textfield.setPromptText("Search here!");
+            //textfield.setPromptText("Search here!");
+
 
             textfield.textProperty().addListener((obs, oldValue, newValue) -> {
                 System.out.println(newValue);
@@ -68,26 +132,34 @@ public class FrontpageController {
                 itemWrapper.getStyleClass().add("item-wrapper");
                 itemWrapper.setAlignment(Pos.CENTER);
 
+
+
                 ImageView imageView = new ImageView();
-                Image image = new Image(MainApplication.class.getResource("/assets/" + recipe.getPicture()).toString());
+
+                String imagePath = "/assets/" + recipe.getPicture();
+                Image image = new Image(
+                        String.valueOf(MainApplication.class.getResource(
+                                imagePath
+                        )));
                 imageView.setImage(image);
                 imageView.setFitWidth(300);
-                imageView.setFitHeight(300);
-                imageView.setPreserveRatio(true);
+                imageView.setFitHeight(180);
+                imageView.setPreserveRatio(false);
 
                 /*ArrayList<Ingredient> ingredientArrayList = recipe.getIngredients();
                 for (int i=0; i < ingredientArrayList.size(); i++) {
                     System.out.println(ingredientArrayList.get(i).getName());
                 }*/
 
-                Hyperlink name = new Hyperlink();
+
+                Label name = new Label();
                 name.setText(recipe.getName());
                 name.setMaxWidth(Double.MAX_VALUE);
                 name.setAlignment(Pos.CENTER);
                 name.getStyleClass().add("item-name");
-                name.setOnAction(new EventHandler<ActionEvent>() {
+                itemWrapper.setOnMouseClicked(new EventHandler<MouseEvent>() {
                     @Override
-                    public void handle(ActionEvent actionEvent) {
+                    public void handle(MouseEvent mouseEvent) {
                         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/recipe-view.fxml"));
                         Parent detailRoot = null;
                         try {
@@ -97,9 +169,11 @@ public class FrontpageController {
                         }
                         DetailPageController detailPageController = loader.getController();
 
-                        // Hier sollten Sie die ausgewählten Rezeptdaten an den DetailseiteController übergeben
+                        int selectedRecipeId = recipe.getRecipeId(); // Hier müssen Sie die ID des ausgewählten Rezepts bekommen
+                        detailPageController.initialize(selectedRecipeId); // Nehmen Sie an, dass die Methode in DetailPageController `loadRecipe` heißt
+
                         try {
-                            detailPageController.getInstructionForRecipe(4);
+                            detailPageController.getInstructionForRecipe(selectedRecipeId);
                         } catch (SQLException e) {
                             throw new RuntimeException(e);
                         }
@@ -129,5 +203,20 @@ public class FrontpageController {
         } catch (SQLException e) {
             db.printSQLException(e);
         }
+    }
+
+    // Methode für die Animation nach oben
+    private void slideUp(Node node) {
+        TranslateTransition slideUpTransition = new TranslateTransition(Duration.millis(500), node);
+        slideUpTransition.setFromY(100);
+        slideUpTransition.setToY(90); // Verändere diesen Wert für die gewünschte Verschiebung
+        slideUpTransition.play();
+    }
+
+    // Methode für die Rückwärtsanimation
+    private void slideDown(Node node) {
+        TranslateTransition slideDownTransition = new TranslateTransition(Duration.millis(500), node);
+        slideDownTransition.setToY(-100); // Setze die Verschiebung zurück
+        slideDownTransition.play();
     }
 }
